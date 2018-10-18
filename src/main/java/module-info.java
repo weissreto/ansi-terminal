12bc290f879418272ae26f1f7268d90cfa95693d
Reto Weiss <reto.weiss@ivyteam.ch>
2018-10-18 22:41:50 +0200

    Initial support for java module system

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ch.rweiss.terminal
+{
+  exports ch.rweiss.terminal;
+  exports ch.rweiss.terminal.graphics;
+  exports ch.rweiss.terminal.table;
+  exports ch.rweiss.terminal.widget;
+  
+  requires ch.rweiss.terminal.nativ;
+  requires ch.rweiss.check;
+}
